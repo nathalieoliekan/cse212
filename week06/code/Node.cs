ddd12010a6d85af3bb4c445ccedc6521dbd5e4ef
nathalieoliekan <nathalie.oliekan@gmail.com>
2025-08-09 11:18:08 -0700

    week 6 code assignment

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,18 +12,22 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
-
-        if (value < Data)
+        if (value == Data)
         {
-            // Insert to the left
+            // No duplicates allowed
+            return;
+        }
+
+        else if (value < Data)
+        {
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else
+
+        else // value > Data
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -31,15 +35,26 @@ public class Node
         }
     }
 
+
+
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data && Left != null)
+            return Left.Contains(value);
+        else if (value > Data && Right != null)
+            return Right.Contains(value);
+        else
+            return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return Math.Max(leftHeight, rightHeight) + 1;
     }
 }
